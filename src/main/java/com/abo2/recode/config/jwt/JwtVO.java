8e455df46fd60335ba8de1f1f0abe4d63ec347fa
백승주 <137677440+bearkuang@users.noreply.github.com>
2024-05-14 21:39:12 +0900

    [Delete] abo2 폴더 삭제

--- src/main/java/com/abo2/recode/config/jwt/JwtVO.java
+++
@@ -1,8 +0,0 @@
-package com.qriz.sqld.config.jwt;
-
-public interface JwtVO {
-    public static final String SECRET = "abo2"; // HS256 (대칭키)
-    public static final int EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 5; // 5일
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER = "Authorization";
-}
